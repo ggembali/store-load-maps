489920a7505c700274c03279191f03e5addae7b6
Ganesh Gembali <ggembali@gmail.com>
2019-04-27 15:16:49 +0530

    Failed tests for ccontract

---
+++ src/main/java/info/ganesh/ArrayMap.java
@@ -0,0 +1,17 @@
+package info.ganesh;
+
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+
+public class ArrayMap {
+
+	public HashMap<String, String>[] load(InputStream inputStream) {
+		return new HashMap[] {};
+	}
+
+	public void store(ArrayMap arrayMap,Writer writer) {
+		
+	}
+
+}
---
+++ src/test/java/info/ganesh/ArrayMapTest.java
@@ -0,0 +1,45 @@
+package info.ganesh;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ArrayMapTest {
+	private InputStream inputStream;
+	private ArrayMap arrayMap = new ArrayMap();
+
+	@Test
+	public void shouldLoadArrayMapFromGivenStream() {
+		inputStream = this.getClass().getResourceAsStream("sampleArrayMap");
+		HashMap<String, String>[] load = arrayMap.load(inputStream);
+		assertThat(load.length, is(3));
+	}
+
+	@Test
+	public void shouldStoreArrayMapIntoAStream() {
+		HashMap<String, String> map = new HashMap<String, String>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				put("key1", "value1");
+				put("key2", "value2");
+			}
+		};
+		
+		Map<String, String>[] arrayMapData = new HashMap[1];
+		arrayMapData[1] = map;
+		StringWriter writer = new StringWriter();
+		arrayMap.store(arrayMap, writer);
+		assertThat(writer.toString(),is("key1=value1;key2=value2"));
+	}
+
+}
